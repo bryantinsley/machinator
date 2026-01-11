ab370c72ba7a1b0b8717a17a67f1176898b3976a
Bryan Tinsley <bryan@tinsley.me>
2026-01-11 17:24:42 -0500

    chore: archive go env setup script after vhs installation

---
+++ _archive/setup_go_env_task.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export GOPATH="$(pwd)/.go-cache"
+export GOBIN="$(pwd)/.go-cache/bin"
+export GOCACHE="$(pwd)/.go-build-cache"
+export GOMODCACHE="$(pwd)/.go-cache/pkg/mod"
+mkdir -p "$GOPATH" "$GOBIN" "$GOCACHE" "$GOMODCACHE"
