d5357679cc02c4a1a22266348b24135117b35e62
Bryan Tinsley <bryan@tinsley.me>
2026-01-11 19:16:35 -0500

    remove _archived and added to .gitignore

--- _archive/2026-01-11/orchestrator_archived/tui_helper.go
+++
@@ -1,6 +0,0 @@
-package main
-
-func getProjectRoot() string {
-	// Use the original cwd captured at startup
-	return originalCwd
-}
--- _archive/2026-01-11/setup_go_env_machinator.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export GOPATH="$(pwd)/.go-cache"
-export GOCACHE="$(pwd)/.go-build-cache"
-export GOMODCACHE="$(pwd)/.go-cache/pkg/mod"
-mkdir -p "$GOPATH" "$GOCACHE" "$GOMODCACHE"
